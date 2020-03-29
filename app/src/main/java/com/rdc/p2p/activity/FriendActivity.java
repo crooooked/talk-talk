94f05cde15429d8f541ed742aa07fca7c23cc6de
crooooked <347028288@qq.com>
2020-03-29 12:04:47 +0800

    conversation by Amber

---
+++ app/src/main/java/com/rdc/p2p/activity/FriendActivity.java
@@ -0,0 +1,8 @@
+package com.rdc.p2p.activity;
+
+import com.rdc.p2p.base.BaseActivity;
+
+public class FriendActivity {
+
+
+}
---
+++ app/src/main/java/com/rdc/p2p/adapter/FriendListRvAdapter.java
@@ -0,0 +1,32 @@
+package com.rdc.p2p.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.rdc.p2p.R;
+import com.rdc.p2p.activity.FriendActivity;
+
+public class FriendListRvAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
